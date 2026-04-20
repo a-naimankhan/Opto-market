81d6f346aa35ec570bbced1494bd74f9ae4e864a
a-naimankhan <naimakhanaibar@gmail.com>
2026-04-21 04:16:20 +0500

    DockerFiles : added skeleton of docker files

---
+++ Client/.angular/cache/21.2.7/optomarket_front/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,44 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-GNLFGYZA.js";
+import "./chunk-WC4PITUE.js";
+import "./chunk-QOW5FBQ3.js";
+import "./chunk-TLKGGICY.js";
+import "./chunk-AHXNCZPV.js";
+import "./chunk-WRI7ZT3C.js";
+import "./chunk-XL6MHRFH.js";
+import "./chunk-N7IPSXXT.js";
+import "./chunk-O5J3CNTX.js";
+import "./chunk-6DU2HRTW.js";
+export {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
